a96d9bff458b32225d90f2d63bbb7e5840de40c4
JJamila <128652768+jjmaila@users.noreply.github.com>
2024-03-04 14:10:01 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,3 @@
 # Bible-Game
-This is a python game and this game is a quiz, where you can answer questions and see if you can do it. This Python Game is called Bible Quiz, you can test your faith. So this game has 10 questions that you can answer and you can see if you are right or wrong. 
+This is a Python game and this game is a quiz, where you can answer questions and see if you can do it. 
+This Python Game is called Bible Quiz, you can test your faith. So this game has 5 questions that you can answer and you can see if you are right or wrong. 
