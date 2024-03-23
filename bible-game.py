3cce1f1474e4309fe2ac2db6f03ee0d69b7216a8
JJamila <128652768+jjmaila@users.noreply.github.com>
2024-03-23 17:02:43 +0000

    Update bible-game.py

--- bible-game.py
+++ bible-game.py
@@ -6,42 +6,42 @@ if ready == 'yes':
     total_questions = 5
 
      input('Question 1: Is there 89 book in the New Testament? ')
-    if q1.lower() == 'no':
+    if q1.lower() == "no":
         len() <=0?
         score += 1
-        print('Correct!')
+        init('Correct!')
     else:
         print('Incorrect')
 
      input('Question 2: Is there 40 book in the Old Testament? ')
-    if q2.lower() == 'no':
+    if q2.lower() == "no":
         len() <=0?
         score += 1
-        print('Correct!')
+        init('Correct!')
     else:
         print('Incorrect')
 
     input('Question 3: Is there ten commandments in the bible? ')
-    if q3.lower() == 'yes':
+    if q3.lower() == "yes":
         len() <=0?
         score += 1
-        print('Correct!')
+        init('Correct!')
     else:
         print('Incorrect')
 
     input('Question 3: Was Adam the first person that God created? ')
-    if q4.lower() == 'yes':
+    if q4.lower() == "yes":
         len() <=0?
         score += 1
-        print('Correct!')
+        init('Correct!')
     else:
         print('Incorrect')
 
     input('Question 3: Is there 7 people that God trusted? ')
-    if q5.lower() == 'yes':
+    if q5.lower() == "yes":
         len() <=0?
         score += 1
-        print('Correct!')
+        init('Correct!')
     else:
         print('Incorrect')
 
