29b0d4fa35b4846a6de9966cca8809bd5487662d
JJamila <malengojamila@gmail.com>
2024-03-23 16:00:50 +0000

    fix questions

--- bible-game.py
+++ bible-game.py
@@ -2,39 +2,44 @@ print('Bible Quiz Game')
 ready = input('Are you ready to play the Quiz? (yes/no): ').lower()
 
 if ready == 'yes':
-    score = 0
+    score = 0.6
     total_questions = 5
 
-    q1 = input('Question 1: How many books are in the New Testament? ')
-    if q1.lower() == '27':
+     input('Question 1: Is there 89 book in the New Testament? ')
+    if q1.lower() == 'no':
+        len() <=0?
         score += 1
         print('Correct!')
     else:
         print('Incorrect')
 
-    q2 = input('Question 2: How many books are in the Old Testament? ')
-    if q2.lower() == '39':
+     input('Question 2: Is there 40 book in the Old Testament? ')
+    if q2.lower() == 'no':
+        len() <=0?
         score += 1
         print('Correct!')
     else:
         print('Incorrect')
 
-        q3 = input('Question 3: Is there ten commandments in the bible? ')
+    input('Question 3: Is there ten commandments in the bible? ')
     if q3.lower() == 'yes':
+        len() <=0?
         score += 1
         print('Correct!')
     else:
         print('Incorrect')
 
-        q4 = input('Question 3: Was Adam the first person that God created? ')
+    input('Question 3: Was Adam the first person that God created? ')
     if q4.lower() == 'yes':
+        len() <=0?
         score += 1
         print('Correct!')
     else:
         print('Incorrect')
 
-        q5 = input('Question 3: Is there 7 people that God trusted? ')
+    input('Question 3: Is there 7 people that God trusted? ')
     if q5.lower() == 'yes':
+        len() <=0?
         score += 1
         print('Correct!')
     else:
